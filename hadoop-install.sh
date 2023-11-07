c965e1dccaafabc482e6f2938bd0a49b02e933b1
srinath <www.srinath.25.com@gmail.com>
2023-11-07 15:36:56 +0530

    upated hadoop-install.sh

--- hadoop-install.sh
+++ hadoop-install.sh
@@ -9,79 +9,59 @@ sudo adduser --ingroup hadoop hadoopusr
 sudo adduser hadoopusr sudo
 
 sudo apt-get install openssh-server
-su - hadoopusr
 
-ssh-keygen -t rsa -P ""
-cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
-ssh localhost
+sudo -u hadoopusr -H ssh-keygen -t rsa -P ""
+sudo -u hadoopusr -H cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
 
-wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz
-sudo tar xvzf hadoop-3.3.6.tar.gz -C /usr/local/
+sudo -u hadoopusr -H ssh localhost
 
-echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> $HOME/.bashrc
-echo "export HADOOP_HOME=/usr/local/hadoop" >> $HOME/.bashrc
-echo "export PATH=$PATH:$HADOOP_HOME/bin" >> $HOME/.bashrc
-echo "export PATH=$PATH:$HADOOP_HOME/sbin" >> $HOME/.bashrc
-echo "export HADOOP_MAPRED_HOME=$HADOOP_HOME" >> $HOME/.bashrc
-echo "export HADOOP_COMMON_HOME=$HADOOP_HOME" >> $HOME/.bashrc
-echo "export HADOOP_HDFS_HOME=$HADOOP_HOME" >> $HOME/.bashrc
-echo "export YARN_HOME=$HADOOP_HOME" >> $HOME/.bashrc
-echo "export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native" >> $HOME/.bashrc
-echo "export HADOOP_OPTS="-Djava.library.path=$HADOOP_HOME/lib"" >> $HOME/.bashrc
-source ~/.bashrc
+sudo -u hadoopusr -H wget https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz
+sudo -u hadoopusr -H tar xvzf hadoop-3.3.6.tar.gz 
+sudo -u hadoopusr -H sudo mv hadoop-3.3.6 hadoop
+sudo -u hadoopusr -H mv hadoop /usr/local/hadoop
 
-echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> $HADOOP_HOME/etc/hadoop/hadoop-env.sh
-
-property="<property>\n\t<name>fs.default.name</name>\n\t<value>hdfs://localhost:9000</value>\n</property>"
-
-sed -i.bak -e "/<\/configuration>/i $property" /usr/local/hadoop/etc/hadoop/core-site.xml
-
-content="<property> 
-<name>dfs.replication</name> 
-<value>1</value> 
-</property> 
-<property> 
-<name>dfs.namenode.name.dir</name> 
-<value>file:/usr/local/hadoop_tmp/hdfs/namenode</value> 
-</property> 
-<property> 
-<name>dfs.datanode.data.dir</name> 
-<value>file:/usr/local/hadoop_tmp/hdfs/datanode</value> 
-</property>"
-
-sed -i.bak -e "/<\/configuration>/i $content" /usr/local/hadoop/etc/hadoop/hdfs-site.xml
-
-content2="
-<property> 
-<name>yarn.nodemanager.aux-services</name> 
-<value>mapreduce_shuffle</value> 
-</property> 
-<property> 
-<name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name> 
-<value>org.apache.hadoop.mapred.ShuffleHandler</value> 
-</property>
-"
-
-sed -i.bak -e "/<\/configuration>/i $content2" /usr/local/hadoop/etc/hadoop/yarn-site.xml
-
-content3="
-<property> 
-<name>mapreduce.framework.name</name> 
-<value>yarn</value> 
-</property>
-"
-
-sed -i.bak -e "/<\/configuration>/i $content2" /usr/local/hadoop/etc/hadoop/mapred-site.xml
+sudo -u hadoopusr -H echo "export java_home=/usr/lib/jvm/java-11-openjdk-amd64" >> $HOME/.bashrc
+sudo -u hadoopusr -H echo "export hadoop_home=/usr/local/hadoop" >> $HOME/.bashrc
+sudo -u hadoopusr -H echo "export path=\$path:\$hadoop_home/bin" >> $HOME/.bashrc
+sudo -u hadoopusr -H echo "export path=\$path:\$hadoop_home/sbin" >> $HOME/.bashrc
+sudo -u hadoopusr -H echo "export hadoop_mapred_home=\$hadoop_home" >> $HOME/.bashrc
+sudo -u hadoopusr -H echo "export hadoop_common_home=\$hadoop_home" >> $HOME/.bashrc
+sudo -u hadoopusr -H echo "export hadoop_hdfs_home=\$hadoop_home" >> $HOME/.bashrc
+sudo -u hadoopusr -H echo "export yarn_home=\$hadoop_home" >> $HOME/.bashrc
+sudo -u hadoopusr -H echo "export hadoop_common_lib_native_dir=\$hadoop_home/lib/native" >> $HOME/.bashrc
+sudo -u hadoopusr -H echo "export hadoop_opts='-djava.library.path=$hadoop_home/lib/native'"
 
 
-sudo mkdir -p /usr/local/hadoop_space
-sudo mkdir -p /usr/local/hadoop_space/hdfs/namenode
-sudo mkdir -p /usr/local/hadoop_space/hdfs/datanode
+sudo -u hadoopusr -H source ~/.bashrc
 
-sudo chown -R hadoopusr /usr/local/hadoop_space
 
-hdfs namenode -format
-start-dfs.sh
-start-yarn.sh
-jps
+sudo -u hadoopusr -H echo "export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64" >> $HADOOP_HOME/etc/hadoop/hadoop-env.sh
+
+
+sudo -u hadoopusr -H property="<property>\n\t<name>fs.default.name</name>\n\t<value>hdfs://localhost:9000</value>\n</property>"
+sudo -u hadoopusr -H sed -i.bak -e "/<\/configuration>/i $property" /usr/local/hadoop/etc/hadoop/core-site.xml
+
+sudo -u hadoopusr -H content="<property>\n\t<name>dfs.replication</name>\n\t<value>1</value>\n</property>\n<property>\n\t<name>dfs.namenode.name.dir</name>\n\t<value>file:/usr/local/hadoop_tmp/hdfs/namenode</value>\n</property>\n<property>\n\t<name>dfs.datanode.data.dir</name>\n\t<value>file:/usr/local/hadoop_tmp/hdfs/datanode</value>\n</property>"
+sudo -u hadoopusr -H sed -i.bak -e "/<\/configuration>/i $content" /usr/local/hadoop/etc/hadoop/hdfs-site.xml
+
+sudo -u hadoopusr -H content2="<property>\n\t<name>yarn.nodemanager.aux-services</name>\n\t<value>mapreduce_shuffle</value>\n</property>\n<property>\n\t<name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>\n\t<value>org.apache.hadoop.mapred.ShuffleHandler</value>\n</property>"
+sudo -u hadoopusr -H sed -i.bak -e "/<\/configuration>/i $content2" /usr/local/hadoop/etc/hadoop/yarn-site.xml
+
+content3="<property>\n\t<name>mapreduce.framework.name</name>\n\t<value>yarn</value>\n</property>"
+sudo -u hadoopusr -H sed -i.bak -e "/<\/configuration>/i $content3" /usr/local/hadoop/etc/hadoop/mapred-site.xml
+
+
+sudo -u hadoopusr -H sudo mkdir -p /usr/local/hadoop_space
+sudo -u hadoopusr -H sudo mkdir -p /usr/local/hadoop_space/hdfs/namenode
+sudo -u hadoopusr -H sudo mkdir -p /usr/local/hadoop_space/hdfs/datanode
+sudo -u hadoopusr -H sudo chown -R hadoopusr /usr/local/hadoop_space
+
+sudo -u hadoopusr -H sudo mkdir -p /usr/local/hadoop_tmp/hdfs/namenode
+sudo -u hadoopusr -H sudo mkdir -p /usr/local/hadoop_tmp/hdfs/datanode
+sudo -u hadoopusr -H sudo chown -R hadoopusr /usr/local/hadoop_tmp
+
+sudo -u hadoopusr -H hdfs namenode -format
+sudo -u hadoopusr -H start-dfs.sh
+sudo -u hadoopusr -H start-yarn.sh
+sudo -u hadoopusr -H jps
 
