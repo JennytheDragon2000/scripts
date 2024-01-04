102bd276e9377a484e58e86069597960cd444afa
srinath <www.srinath.25.com@gmail.com>
2024-01-04 16:05:18 +0530

    modified the sub.py file

--- sub.py
+++ sub.py
@@ -1,13 +1,13 @@
 #! /usr/bin/python3
 
 import sys
+from os import stat
 
 import pysrt
 
 subfile = sys.argv[1]
 startNumber = int(sys.argv[2])
 subTextEnd = sys.argv[3:]
-# endNumber = 8
 subs = pysrt.open(subfile)
 # print(subs[5])
 # print(subs[5].start.minutes)
@@ -15,14 +15,30 @@ subs = pysrt.open(subfile)
 # print(subs[5].start.milliseconds)
 # print(subs[5].text)
 
+# with open("extractedText", "a") as file:
+#     file.write(f"{startNumber}\n")
+#     for index, sub in enumerate(subs[startNumber:]):
+#         # if sub.text == subTextEnd:
+#         if any(subText in sub.text for subText in subTextEnd):
+#             print(subs[index])
+#             file.write(str(index))
+#             with open("endNumber.txt", "w") as file2:
+#                 file2.write(str(index + 1))
+#             break
+#         file.write(f"{sub.text}\n")
+
+
+for index, sub in enumerate(subs):
+    # if sub.text == subTextEnd:
+    if any(subText in sub.text for subText in subTextEnd):
+        endNumber = index
+        print(startNumber)
+        print(endNumber)
+        break
+
 with open("extractedText", "a") as file:
-    file.write(f"{startNumber}\n")
-    for index, sub in enumerate(subs[startNumber + 1 :]):
-        # if sub.text == subTextEnd:
-        if any(subText in sub.text for subText in subTextEnd):
-            print(subs[index])
-            file.write(str(index))
-            with open("endNumber.txt", "w") as file2:
-                file2.write(str(index))
-            break
-        file.write(f"{sub.text}\n")
+    for sub in subs[startNumber:endNumber]:
+        file.write(f"{sub.text}")
+
+with open("endNumber.txt", "w") as file:
+    file.write(str(endNumber))
